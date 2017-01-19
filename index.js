4f1352c3e62a30a206a40e43fe5a50d6da4027fe
Mees van Dijk <mees@itisme.es>
2017-01-19 22:17:13 +0100

    split eslint and react rules and changed rules

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const eslint = require('./rules/eslint')
+const react = require('./rules/react')
+
 module.exports = {
   extends: [
     'eslint:all',
@@ -9,27 +12,8 @@ module.exports = {
     'react',
     'import'
   ],
-  rules: {
-    'capitalized-comments': 'off',
-    'dot-location': ['error', 'property'],
-    indent: ['error', 2],
-    'init-declarations': 'off',
-    'newline-after-var': 'off',
-    'no-console': 'off',
-    'no-magic-numbers': 'off',
-    'no-negated-condition': 'off',
-    'no-process-env': 'off',
-    'no-unused-vars': 'warn',
-    'object-curly-spacing': ['error', 'always'],
-    'one-var': ['error', 'never'],
-    'padded-blocks': ['error', 'never'],
-    'quote-props': ['error', 'as-needed'],
-    quotes: ['error', 'single'],
-    radix: ['warn', 'as-needed'],
-    semi: ['error', 'never'],
-    'sort-imports': 'off',
-    'sort-keys': 'off',
-    strict: 'off',
-    'template-curly-spacing': ['error', 'always']
-  }
+  rules: Object.assign(Object.create(null),
+    eslint,
+    react
+  )
 }
---
+++ rules/react.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'react/display-name': 'warn',
+  'react/jsx-filename-extension': ['warn', { extensions: ['js'] }]
+}
