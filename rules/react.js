dfa001d5a8793123c23e2d8d98b5078a25949287
Mees van Dijk <mees@itisme.es>
2017-01-20 12:14:02 +0100

    changed rule jsx-indent

--- rules/react.js
+++ rules/react.js
@@ -1,4 +1,5 @@
 module.exports = {
   'react/display-name': 'warn',
-  'react/jsx-filename-extension': ['warn', { extensions: ['js'] }]
+  'react/jsx-filename-extension': ['warn', { extensions: ['js'] }],
+  'react/jsx-indent': ['error', 2]
 }
