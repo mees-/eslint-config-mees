ab848517173c55c0b3640b7d7b7107ed9f42cf43
Mees van Dijk <mees@itisme.es>
2017-01-20 12:17:16 +0100

    changed rule react/jsx-no-literals

--- rules/react.js
+++ rules/react.js
@@ -1,5 +1,6 @@
 module.exports = {
   'react/display-name': 'warn',
   'react/jsx-filename-extension': ['warn', { extensions: ['js'] }],
-  'react/jsx-indent': ['error', 2]
+  'react/jsx-indent': ['error', 2],
+  'react/jsx-no-literals': 'off'
 }
