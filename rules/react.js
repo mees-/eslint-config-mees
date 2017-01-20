c03d0523efa4f5dce18b9fe9f7991cc4b33b6a5b
Mees van Dijk <mees@itisme.es>
2017-01-21 00:59:11 +0100

    changed rule require-optimization

--- rules/react.js
+++ rules/react.js
@@ -2,5 +2,6 @@ module.exports = {
   'react/display-name': 'warn',
   'react/jsx-filename-extension': ['warn', { extensions: ['js'] }],
   'react/jsx-indent': ['error', 2],
-  'react/jsx-no-literals': 'off'
+  'react/jsx-no-literals': 'off',
+  'react/require-optimization': 'off'
 }
