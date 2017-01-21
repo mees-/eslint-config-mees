d79e9b458e281d5c90673bf325b398dd8fa2e1a5
Mees van Dijk <mees@itisme.es>
2017-01-21 02:09:17 +0100

    changed rule react/jsx-handler-name

--- rules/react.js
+++ rules/react.js
@@ -2,6 +2,7 @@ module.exports = {
   'react/display-name': 'warn',
   'react/jsx-curly-spacing': ['error', 'always'],
   'react/jsx-filename-extension': ['warn', { extensions: ['js'] }],
+  'react/jsx-handler-names': 'off',
   'react/jsx-indent': ['error', 2],
   'react/jsx-no-literals': 'off',
   'react/prop-types': 'off',
