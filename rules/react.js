53d1bd8c0ed2ddf312060e5f486d6b024bda1d77
Mees van Dijk <mees@itisme.es>
2017-01-21 02:24:30 +0100

    changed rule react/allowArrowFunctions

--- rules/react.js
+++ rules/react.js
@@ -4,6 +4,9 @@ module.exports = {
   'react/jsx-filename-extension': ['warn', { extensions: ['js'] }],
   'react/jsx-handler-names': 'off',
   'react/jsx-indent': ['error', 2],
+  'react/jsx-no-bind': ['warn', {
+    allowArrowFunctions: true
+  }],
   'react/jsx-no-literals': 'off',
   'react/prop-types': 'off',
   'react/require-optimization': 'off'
