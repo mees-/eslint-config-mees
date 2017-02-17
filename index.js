9a81fba5588947ff182398c82f0f827deab7c7f5
Mees van Dijk <mees@itisme.es>
2017-02-17 20:28:10 +0100

    changed rule id-length

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ module.exports = {
       'capitalized-comments': 'off',
       'class-methods-use-this': 'off',
       'dot-location': ['error', 'property'],
+      'id-length': 'off',
       indent: ['error', 2, {
         SwitchCase: 1
       }],
